74ec307efceba1c5d0cd00d101273bbc622289df
Kanysh <127661635+hush0k@users.noreply.github.com>
2024-04-09 02:38:10 +0500

    Update paint.py

--- Labs/lab9/paint/paint.py
+++ Labs/lab9/paint/paint.py
@@ -26,8 +26,9 @@ PURPLE = (221,160,221)
 def drawRect(color, pos, width, height):
     pygame.draw.rect(screen, color, (pos[0], pos[1], width, height), 4)
 
-def drawCircle(color, pos, RAD):
-    pygame.draw.circle(screen, color, pos, RAD, 4)
+def drawCircle(color, pos, x, y):
+    rad = math.sqrt(x ** 2 + y ** 2)
+    pygame.draw.circle(screen, color, pos, rad, 4)
 
 def eraser(pos, RAD):
     pygame.draw.circle(screen, WHITE, pos, RAD)
@@ -35,29 +36,39 @@ def eraser(pos, RAD):
 def drawSquare(color, pos, side_length):
     pygame.draw.rect(screen, color, (pos[0], pos[1], side_length, side_length), 4)
 
-def drawRightTriangle(color, pos, base, height):
-    points = [(pos[0], pos[1] + height), (pos[0] + base, pos[1] + height), (pos[0] + base, pos[1])]
+def drawRightTriangle(color, start, end):    
+    points = [(start[0], start[1]), (start[0], end[1]), (end[0], end[1])]
     pygame.draw.polygon(screen, color, points, 4)
 
-def drawEquilateralTriangle(color, pos, side_length):
-    height = side_length * math.sqrt(3) / 2
-    points = [(pos[0], pos[1] + height), (pos[0] + side_length / 2, pos[1]), (pos[0] + side_length, pos[1] + height)]
+def drawEquilateralTriangle(color, start, end):
+    # c_x = (start[0] + end[0]) / 2
+    # c_y = (start[1] + end[1]) / 2
+
+    # r = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2) * math.sqrt(3) / 3
+
+    # x3 = c_x + r * math.cos(math.radians(120))
+    # y3 = c_y + r * math.sin(math.radians(120))
+
+    del_x = end[0] - start[0]
+    del_y = end[1] - start[1]
+    x3 = end[0] + del_x / 2 + math.sqrt(3)/2 * del_y
+    y3 = end[1] + del_y / 2 + math.sqrt(3)/2 * del_x
+    points = [(start[0], start[1]), (x3, y3), (end[0], end[1])]
     pygame.draw.polygon(screen, color, points, 4)
 
-def drawRhombus(color, pos, side_length, angle):
-    half_diagonal = side_length / 2
+def drawRhombus(color, pos, x, y):
+    half_x = x / 2
+    half_y = y / 2
     points = [
-        (pos[0] + half_diagonal, pos[1]), 
-        (pos[0] + side_length, pos[1] + half_diagonal), 
-        (pos[0] + half_diagonal, pos[1] + side_length), 
-        (pos[0], pos[1] + half_diagonal)
+        (pos[0] + half_x, pos[1]),
+        (pos[0], pos[1] + half_y),
+        (pos[0] + half_x, pos[1] + y),
+        (pos[0] + x, pos[1] + half_y)
     ]
-    rotated_points = []
-    for point in points:
-        rotated_x = pos[0] + (point[0] - pos[0]) * math.cos(math.radians(angle)) - (point[1] - pos[1]) * math.sin(math.radians(angle))
-        rotated_y = pos[1] + (point[0] - pos[0]) * math.sin(math.radians(angle)) + (point[1] - pos[1]) * math.cos(math.radians(angle))
-        rotated_points.append((rotated_x, rotated_y))
-    pygame.draw.polygon(screen, color, rotated_points, 4)
+    
+    
+    
+    pygame.draw.polygon(screen, color, points, 4)
 
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
@@ -72,6 +83,7 @@ RAD = 30
 
 drawing = False
 color = BLACK
+previous_color = BLACK
 
 screen.fill(pygame.Color('white'))
 rainbow = pygame.image.load('color.jpg')
@@ -88,7 +100,7 @@ mode = 0
 # 2 - Eraser
 # 3 - Pencil
 
-mode_name = ['rect', 'circle', 'eraser', 'pencil', 'square', 'right tri', 'equil tri']
+mode_name = ['rect', 'circle', 'eraser', 'pencil', 'square', 'right tri', 'equil tri', 'rhombus']
 
 img_cnt = 0
 
@@ -119,17 +131,18 @@ while not finished:
             if mode == 0:
                 drawRect(color, start_pos, rect_x, rect_y)
             elif mode == 1:
-                drawCircle(color, start_pos, rect_x)
+                drawCircle(color, start_pos, rect_x, rect_y)
             elif mode == 4:
                 side_length = max(rect_x, rect_y)
                 drawSquare(color, start_pos, side_length)
             elif mode == 5:
-                drawRightTriangle(color, start_pos, rect_x, rect_y)
+                drawRightTriangle(color, start_pos, end_pos)
             elif mode == 6:
                 side_length = min(rect_x, rect_y)
-                drawEquilateralTriangle(color, start_pos, side_length)
+                drawEquilateralTriangle(color, start_pos, end_pos)
             elif mode == 7:
-                drawRhombus(color, start_pos, rect_x, 45)
+                drawRhombus(color, start_pos, rect_x, rect_y)
+                
 
         if event.type == pygame.MOUSEMOTION and drawing:
             if mode == 2:
@@ -142,9 +155,12 @@ while not finished:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 mode += 1
-                mode %= 7
+                mode %= 8
                 if mode == 2:
+                    previous_color = color
                     color = WHITE
+                else:
+                    color = previous_color
             if event.key == pygame.K_BACKSPACE:
                 screen.fill(pygame.Color('white'))
 
